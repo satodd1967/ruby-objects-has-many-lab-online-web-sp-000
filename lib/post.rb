3706cbf6d8123fb1c93dd5a0db6aea808cf2c407
Seth Todd <seth.todd@outlook.com>
2020-03-08 03:27:12 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,25 @@
+require "pry"
+class Post
+  
+  attr_accessor :title, :author
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def author_name
+    self.author.name
+  end
+  binding.pry
+end
---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+require "pry"
+class Song
+  attr_accessor :name, :artist
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all << self
+  end
+#binding.pry
+end
